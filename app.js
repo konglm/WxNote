7c39d184bd36d332f333faafa98b4822ecf864ca
konglm <klm.821222@163.com>
2017-07-05 17:27:07 +0800

    增加样式（weui）

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 //app.js
 App({
   onLaunch: function() {
+    // if (wx.openBluetoothAdapter) {
+    //   wx.openBluetoothAdapter()
+    // } else {
+    //   // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+    //   wx.showModal({
+    //     title: '提示',
+    //     content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+    //   })
+    // }
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -10,7 +19,7 @@ App({
   getUserInfo: function(cb) {
     var that = this
     if (this.globalData.userInfo) {
-      typeof cb == "function" && cb(this.globalData.userInfo)
+      typeof cb == "function" && cb(this.globalData.userInfo)//如果参数cb类型为函数，执行cb,获取用户信息
     } else {
       //调用登录接口
       wx.getUserInfo({
